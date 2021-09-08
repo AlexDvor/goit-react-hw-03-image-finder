577bd31dc1671f7cf5bb81a8bc898191de798679
Oleksii Matviichuk <alexmatvichuk@gmail.com>
2021-09-08 18:16:18 +0200

    added prettir.. + searchbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-function App() {
-  return <></>;
+import { Component } from 'react';
+
+// Components
+
+import { Searchbar } from './components/Searchbar/Searchbar';
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <>
+        <Searchbar></Searchbar>
+      </>
+    );
+  }
 }
 
 export default App;
